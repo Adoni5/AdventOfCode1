42c86b17fd39cb8f76e4366bb4a78e36d64e1c91
Adoni5 <roryjmunro1@gmail.com>
2022-12-13 09:03:49 +0000

    P2 wokring 🦌 (after a cheeky hint from alex about functool cmp_to_key)

--- 2022/python/day13/day13.py
+++ 2022/python/day13/day13.py
@@ -26,50 +26,55 @@ test_input = """[1,1,3,1,1]
 [1,[2,[3,[4,[5,6,7]]]],8,9]
 [1,[2,[3,[4,[5,6,0]]]],8,9]"""
 
-# test_input = get_input("13")
+test_input = get_input("13")
 
-def compare_items(cl, cr):
+def compare_items(cl, cr, p2=True):
     """COmpare items"""
     for cl, cr in zip_longest(cl, cr):
         # print(cl, cr)
         if cl is None:
-            return True
+            return True if not p2 else -1
         if cr is None:
-            return False
+            return False if not p2 else + 1
         # both ints
         if isinstance(cl, int) and isinstance(cr, int):
             # print(f"both ints {cl} {cr}")
             if cl == cr:
                 continue
-            return cl < cr
+            if p2:
+                return -1 if cl < cr else + 1
+            else:
+                return cl < cr
         #one of them is a list
         elif isinstance(cl, int) or isinstance(cr, int):
             # print("gello")
             # cr is the list
             if isinstance(cl, int):
-                ans = compare_items([cl], cr)
+                ans = compare_items([cl], cr, p2)
             else:
-                ans = compare_items(cl, [cr])
+                ans = compare_items(cl, [cr], p2)
             if ans is None:
                 continue
             return ans
         # both lists
         else:
             # print(f"both lists {cl} {cr}")
-            ans = compare_items(cl, cr)
+            ans = compare_items(cl, cr, p2)
             if ans is None:
                 continue
             return ans
 sums = []
 for i, pair in enumerate(map(str.split, test_input.split("\n\n")), start=1):
     left, right = map(eval, pair)
-    print(left, right)
-    sums.append(i * compare_items(left, right))
+    sums.append(i * compare_items(left, right, p2=False))
 print(sum(sums))
 
 
 # p2
 p2_test_input = test_input.replace("\n\n", "\n")
+p2_test_input += """
+[[2]]
+[[6]]"""
 # print(p2_test_input)
-print(sorted(map(eval, p2_test_input.splitlines()), key=cmp_to_key(compare_items)))
-print(list(map(eval, p2_test_input.splitlines())))
+p2 = sorted(map(eval, p2_test_input.splitlines()), key=cmp_to_key(compare_items))
+print((p2.index([[2]])+1) * (p2.index([[6]])+1))
