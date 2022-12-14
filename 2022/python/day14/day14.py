cd4b53af2a110a95e67afd34790c5d0cd6699e87
Adoni5 <roryjmunro1@gmail.com>
2022-12-14 10:01:07 +0000

    For christmas, I would like the ability to read puzzle questions properly 🌟🌟 Luckily, I enjoyed this one a lot anyway!

--- 2022/python/day14/day14.py
+++ 2022/python/day14/day14.py
@@ -1,5 +1,5 @@
 import re
-from support import sliding_window
+from support import sliding_window, get_input
 from pprint import pprint
 test_input = """498,4 -> 498,6 -> 496,6
 503,4 -> 502,4 -> 502,9 -> 494,9"""
@@ -10,101 +10,103 @@ def pgprint(pg):
     """Print a nested list of lists as a grid"""
     print("\n".join(["".join(list(map(str, p))) for p in pg]))
 
-listy = [["."] * 10 for _ in range(10)]
 
 rocks = set()
 sand = set()
-
+test_input = get_input("14")
 for line in test_input.splitlines():
     itery = sliding_window(pat.findall(line), 4, skip=True)
     for coords in itery:
         x1, y1, x2, y2 = map(int, coords)
         for x in range(min(x1, x2), max(x1, x2) +1):
             # print(f"x is {x-494}")
-            listy[y1][x-494] = "#"
+            # listy[y1][x-494] = "#"
             rocks.add((x, y1))
         for y in range(min(y1, y2), max(y1, y2)+1):
             # print(f"y is {y}")
-            listy[y][x1-494] = "#"
+            # listy[y][x1-494] = "#"
             rocks.add((x1, y))
 
 
         next(itery)
-pgprint(listy)
+# listy = [["."] *  for _ in range(10)]
+
+# pgprint(listy)
         
 sandy = True
 sand_start = 500, 0
 sand_count = 0
 lowest_rock = max(r[1] for r in iter(rocks))
 print(lowest_rock)
-pprint(rocks)
+infinte_floor = lowest_rock + 2
+# pprint(rocks)
+sand_char = "o"
+max_x, min_x = max(r[0] for r in iter(rocks)), min(r[0] for r in iter(rocks))
+for i in range(min_x - 5000, max_x + 5000):
+    rocks.add((i, infinte_floor))
+
 
 while sandy:
     sand_x, sand_y = sand_start
-    print(f"start {sand_x}, {sand_y}")
+    # print(f"start {sand_x}, {sand_y}")
     # sand falls everybody drops
     while True:
         sand_down = (sand_x, sand_y+1)
-        print(sand_down)
-        if sand_down in rocks and sand_down not in sand:
-            print("hit rock")
-            sand.add((sand_x, sand_y))
-            break
-        if sand_down in sand:
-            print("hit sand")
+        # print(sand_down)
+        # STUPID
+        # if sand_down in rocks and sand_down not in sand:
+        #     print("hit rock")
+        #     sand.add((sand_x, sand_y))
+        #     listy[sand_y][sand_x-494] = sand_char
+
+        #     break
+        if sand_down in sand or sand_down in rocks:
+            # print("hit obstacle")
             sand_down_left = (sand_x - 1 , sand_y + 1)
             sand_down_right = (sand_x + 1, sand_y + 1)
             if sand_down_left not in rocks and sand_down_left not in sand:
-                # try falling again
-                print("moving left")
-                sdl_x, sdl_y = sand_down_left
-                while (sdl_x - 1, sdl_y + 1) not in rocks and (sdl_x - 1, sdl_y + 1) not in sand:
-                    print("Moved to the left")
-                    sdl_x -= 1
-                    sdl_y += 1
-                    if sdl_y > lowest_rock:
-                        print("fell off")
-                        sandy = False
-                        break
-                if sandy:
-                    sand.add((sdl_x, sdl_y))
-                break
+                # print("moving left")
+                sand_x -= 1
+                if sand_y > infinte_floor:
+                    print("fell off")
+                    sandy = False
+                    break
+                continue
             elif sand_down_right not in rocks and sand_down_right not in sand:
-                print("moving right")
+                # print("moving right")
 
-                sdr_x, sdr_y = sand_down_right
-                while (sdr_x + 1, sdr_y + 1) not in rocks and (sdr_x + 1, sdr_y + 1) not in sand:
-                    print("Moved to the right")
-                    sdr_x -= 1
-                    sdr_y += 1
-                    if sdr_y > lowest_rock:
-                        print("fell off")
-                        sandy = False
-                        break
+                sand_x += 1
 
-                if sandy:
-                    sand.add((sdr_x, sdr_y))
-                break
+                if sand_y > infinte_floor:
+                    print("fell off")
+                    sandy = False
+                    break
+                continue
             # It's now resting on 3 sand
             else:
-                print("resting on sand")
+                # print("resting on sand")
+                # listy[sand_y][sand_x-494] = sand_char
+
                 sand.add((sand_x, sand_y))
+                if (sand_x, sand_y) == (500,0):
+                    sand_count += 1
+                    sandy = False
                 break
         
             
         sand_y += 1 
-        if sand_y > lowest_rock:
+        if sand_y > infinte_floor:
             print("fell off")
             sandy = False
             break
-    sand_count += 1
-print(sand)
+    if sandy:
+        sand_count += 1
+    # pgprint(listy)
+# print(sand)
 print(sand_count)
-sand_char = "o"
 
-for x, y in iter(sand):
-    print(x, y)
-    listy[y][x-494] = sand_char
-pgprint(listy)
+# for x, y in iter(sand):
+#     listy[y][x-494] = sand_char
+# pgprint(listy)
 
 
