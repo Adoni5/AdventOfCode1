6eefb04494f9bf6229469f20dd6a0508e4b0e074
Adoni5 <roryjmunro1@gmail.com>
2023-10-21 13:19:27 +0100

    Day 22 P2 what a waste of time Hardcoding is bad for the soul

--- 2022/python/day22/day22.py
+++ 2022/python/day22/day22.py
@@ -19,6 +19,7 @@ test_input = get_input(22, strip=False)
 grid, instructions = test_input.split("\n\n")
 grid = grid.split("\n")
 print(grid)
+# pad into cube
 width = max(len(row) for row in grid)
 
 grid = [row + " " * (width - len(row)) for row in grid]
@@ -71,3 +72,76 @@ else:
     else:
         d = 3
 print(sum((1000 * (r + 1), 4 * (c + 1), d)))
+
+# Part 3 the hardcodening
+c, r = grid[0].index("."), 0
+dc, dr = 1, 0
+for steps, turn in instructions:
+    for x in range(int(steps)):
+        cdr = dr
+        cdc = dc
+        nc = c + dc
+        nr = r + dr
+        # move up off of first face
+        if nr < 0 and 50 <= nc < 100 and dr == -1:
+            dr, dc = 0, 1
+            nr, nc = nc + 100, 0
+        elif nc < 0 and 150 <= nr < 200 and dc == -1:
+            dr, dc = 1, 0
+            nr, nc = 0, nr - 100
+        elif nr < 0 and 100 <= nc < 150 and dr == -1:
+            nr, nc = 199, nc - 100
+        elif nr >= 200 and 0 <= nc < 50 and dr == 1:
+            nr, nc = 0, nc + 100
+        elif nc >= 150 and 0 <= nr < 50 and dc == 1:
+            dc = -1
+            nr, nc = 149 - nr, 99
+        elif nc == 100 and 100 <= nr < 150 and dc == 1:
+            dc = -1
+            nr, nc = 149 - nr, 149
+        elif nr == 50 and 100 <= nc < 150 and dr == 1:
+            dr, dc = 0, -1
+            nr, nc = nc - 50, 99
+        elif nc == 100 and 50 <= nr < 100 and dc == 1:
+            dr, dc = -1, 0
+            nr, nc = 49, nr + 50
+        elif nr == 150 and 50 <= nc < 100 and dr == 1:
+            dr, dc = 0, -1
+            nr, nc = nc + 100, 49
+        elif nc == 50 and 150 <= nr < 200 and dc == 1:
+            dr, dc = -1, 0
+            nr, nc = 149, nr - 100
+        elif nr == 99 and 0 <= nc < 50 and dr == -1:
+            dr, dc = 0, 1
+            nr, nc = nc + 50, 50
+        elif nc == 49 and 50 <= nr < 100 and dc == -1:
+            dr, dc = 1, 0
+            nr, nc = 100, nr - 50
+        elif nc == 49 and 0 <= nr < 50 and dc == -1:
+            dc = 1
+            nr, nc = 149 - nr, 0
+        elif nc < 0 and 100 <= nr < 150 and dc == -1:
+            dc = 1
+            nr, nc = 149 - nr, 50
+        if grid[nr][nc] == "#":
+            dr = cdr
+            dc = cdc
+            break
+        c = nc
+        r = nr
+    if turn == "R":
+        dc, dr = -dr, dc
+    elif turn == "L":
+        dc, dr = dr, -dc
+d = 0
+if dr == 0:
+    if dc == 1:
+        d = 0
+    else:
+        d = 2
+else:
+    if dr == 1:
+        d = 1
+    else:
+        d = 3
+print(1000 * (r + 1) + 4 * (c + 1) + d)
