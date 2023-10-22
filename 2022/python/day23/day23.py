c8d195a64216d65c71985485c9d5892a6793cd23
Adoni5 <roryjmunro1@gmail.com>
2023-10-22 18:35:45 +0100

    Day 23 not too bad! ✨

--- 2022/python/day23/day23.py
+++ 2022/python/day23/day23.py
@@ -1,44 +1,100 @@
-test_input = """.....
-..##.
-..#..
-.....
-..##.
-....."""
+test_input = """..............
+..............
+.......#......
+.....###.#....
+...#...#.#....
+....#...##....
+...#.###......
+...##.#.##....
+....#..#......
+..............
+..............
+.............."""
 
 from collections import deque
-from support import Grid
+from support import Grid, get_input
 
 # N, S, W, E
-d = deque([((-1, -1), (0, -1), (1, -1)), ((-1, 1), (0, 1), (1, 1)), ((-1, 1), (-1, 0), (-1, -1)), ((1, 1), (1, 0), (1, -1))])
+d = deque(
+    [
+        ((-1, -1), (0, -1), (1, -1)),
+        ((-1, 1), (0, 1), (1, 1)),
+        ((-1, -1), (-1, 0), (-1, 1)),
+        ((1, 1), (1, 0), (1, -1)),
+    ]
+)
+test_input = get_input(23, strip=False)
 g = Grid.read_grid(test_input)
 
-starting_elves = g.find_all_items("#")
-print(starting_elves)
-
-print(g.grid)
+elves = g.find_all_items("#")
+elf_check = set(elves)
 go = True
-elf_num = 1
+step = 1
 while go:
-    proposed = set()
-    for elf in starting_elves:
-        print("elf", elf, elf_num)
-        for coord, neighbours in g.get_neighbours(elf, diagonal=True, enforce_edges=False, return_coords=True, fill_value=".").items():
+    # print(f"step {step + 1}\n")
+    failed = set()
+    proposed = {}
+    stop = 0
+    c = 0
+    for index, elf in enumerate(elves):
+        for coord, neighbours in g.get_neighbours(
+            elf, diagonal=True, enforce_edges=False, return_coords=True, fill_value="."
+        ).items():
             x, y = coord
-            # print(coord)
-            for direction in d:
-                # for dx, dy in direction:
-                #     print(dx, dy)
-                #     print((x + dx, y + dy))
-                #     print(g.grid.get((x + dx, y + dy), "."))
-                if all((g.grid.get((x + dx, y + dy), ".") != "#" for dx, dy in direction)):
-                    print("hello")
-                    print(coord, (x + direction[1][0], y + direction[1][1]))
-                    proposed.add((x + direction[1][0], y + direction[1][1]))
+            if all(
+                (
+                    (x + dx, y + dy) not in elf_check
+                    for direction in d
+                    for dx, dy in direction
+                )
+            ):
+                # print(
+                #     f"elf {index} at {elf} has no neighbours, breaking",
+                # )
+                c += 1
+                if c == len(elves):
+                    go = False
                     break
-                else:
-                    print("dead")
-        elf_num += 1
-        # break
+                break
+            for direction in d:
+                # print(f"checking direction {direction} for elf {index} at {elf}")
+                if all(((x + dx, y + dy) not in elf_check for dx, dy in direction)):
+                    # print(
+                    #     "proposed for",
+                    #     coord,
+                    #     (x + direction[1][0], y + direction[1][1]),
+                    # )
+                    # print(f"ELF {index} at {elf} moving to direction {direction}")
+                    if (x + direction[1][0], y + direction[1][1]) not in proposed and (
+                        x + direction[1][0],
+                        y + direction[1][1],
+                    ) not in failed:
+                        proposed[(x + direction[1][0], y + direction[1][1])] = index
+                    else:
+                        proposed.pop((x + direction[1][0], y + direction[1][1]))
+                        failed.add((x + direction[1][0], y + direction[1][1]))
 
-    print(proposed)
-    break
+                    break
+    if c != len(elves):
+        for proposal, elf in proposed.items():
+            elves[elf] = proposal
+        # print(f"elves now {elves}")
+        d.rotate(-1)
+        # print(f"direction now {d}\n")
+        step += 1
+    elf_check = set(elves)
+    # -1 as we are adding one without executing above
+    if step - 1 == 10:
+        # print(elves)
+        bounds = (
+            min(elves, key=lambda x: x[0])[0],
+            min(elves, key=lambda x: x[1])[1],
+            max(elves, key=lambda x: x[0])[0],
+            max(elves, key=lambda x: x[1])[1],
+        )
+        print(
+            f"part 1 {(bounds[2] + 1 - bounds[0]) * (bounds[3] + 1 - bounds[1]) - len(elves)}"
+        )
+
+
+print(f"part 2 {step}")
