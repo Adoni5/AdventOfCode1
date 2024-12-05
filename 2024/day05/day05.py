6b289b87ffb9fde27c09e2cc52426662435af5e7
Adoni5 <roryjmunro1@gmail.com>
2024-12-05 08:47:16 +0000

    Merry christmas! P2 making heavy weather out of using pop left leaving me with no correctly reordered deque Solid 2 minutes when it came to me

--- 2024/day05/day05.py
+++ 2024/day05/day05.py
@@ -1,4 +1,5 @@
 from collections import defaultdict, deque
+from itertools import islice
 
 test_string = """47|53
 97|13
@@ -39,18 +40,36 @@ for rule in rules.strip().splitlines():
     a, b = tuple(map(int, rule.split("|")))
     r[a].append(b)
 p1 = 0
+p2 = 0
 for o in ordering.strip().splitlines():
+    ordered = []
+    correct = True
     ns = deque(map(int, o.split(",")))
     _ns = ns.copy()
+    popped = -1
     while _ns:
         n = _ns.popleft()
+        popped += 1
         rs = r[n]
         if not set(_ns).issubset(set(rs)):
-            # print(_ns)
-            break
-    # print(o, _ns)
-    if not _ns:
-        # print(int((len(ns) - 1) / 2))
+            correct = False
+            _correct = False
+            i = 0
+            while not _correct:
+                _ns.insert(i, n)
+                if set(list(_ns)[i + 1 :]).issubset(set(rs)):
+                    _correct = True
+
+                    break
+                else:
+                    del _ns[i]
+                i += 1
+        else:
+            ordered.append(n)
+    if correct:
         p1 += ns[int((len(ns) - 1) / 2)]
+    else:
+        p2 += ordered[int((len(ordered) - 1) / 2)]
 
 print(p1)
+print(p2)
