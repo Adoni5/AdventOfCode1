5070ae8122974058fa76805fc31d043933e5acd9
Adoni5 <roryjmunro1@gmail.com>
2024-12-06 08:28:07 +0000

    Part two pulling into portsmouth 🎄🎄

--- 2024/day06/day06.py
+++ 2024/day06/day06.py
@@ -22,28 +22,39 @@ for r, line in enumerate(test_input.strip().splitlines()):
             start = (c, r)
         g[c, r] = n
 # print(start)
-check = []
-ds = deque([(0, -1), (1, 0), (0, 1), (-1, 0)])
-
-visited = set()
-visited.add((start[0], start[1], ds[0][0], ds[0][1]))
-c, r = start
-while True:
-    dc, dr = ds[0]
-    nc, nr = c + dc, r + dr
-    if (nc, nr, dc, dr) in visited:
-        break
-    if g.get((nc, nr), "X") != "#":
-        if g.get((nc, nr), "X") == "X":
-            break
-        visited.add((nc, nr, dc, dr))
-        check.append((nc, nr))
-        c, r = nc, nr
+obstacles = []
+loop = True
+it = deque(g.keys())
+while loop:
+    c, r = start
+    ds = deque([(0, -1), (1, 0), (0, 1), (-1, 0)])
+    visited = set()
+    visited.add((start[0], start[1], ds[0][0], ds[0][1]))
+    og = None
+    if it:
+        obs = it.popleft()
+        og = g.get(obs)
+        g[obs] = "#"
     else:
-        # print("\n".join([f"{a}, {b}" for a, b in check]))
-        check = []
-        # print()
-        ds.rotate(-1)
+        break
+    while True:
+        dc, dr = ds[0]
+        nc, nr = c + dc, r + dr
+        if (nc, nr, dc, dr) in visited:
+            obstacles.append(obs)
+            break
+        if g.get((nc, nr), "X") != "#":
+            if g.get((nc, nr), "X") == "X":
+                # print(f"left map with new obstacle at {obs} ")
+                break
+            visited.add((nc, nr, dc, dr))
+            c, r = nc, nr
+        else:
+            ds.rotate(-1)
+    g[obs] = og
 
-print(visited)
+# print(visited)
 print(len(set((a, b) for a, b, _, _ in visited)))
+
+print(obstacles)
+print(len(obstacles))
