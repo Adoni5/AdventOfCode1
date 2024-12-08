118d359ad3355c196a18856305f27976d3b5d0e3
Adoni5 <roryjmunro1@gmail.com>
2024-12-08 13:01:32 +0000

    part 2 - haven't felt this smart in a long time! Thank you santaaaa🎅🎅🎅

--- 2024/day08/day08.py
+++ 2024/day08/day08.py
@@ -1,15 +1,15 @@
-test_input = """............
-........0...
-.....0......
-.......0....
-....0.......
-......A.....
-............
-............
-........A...
-.........A..
-............
-............"""
+test_input = """##....#....#
+.#.#....0...
+..#.#0....#.
+..##...0....
+....0....#..
+.#...#A....#
+...#..#.....
+#....#.#....
+..#.....A...
+....#....A..
+.#........#.
+...#......##"""
 
 from itertools import permutations
 from collections import defaultdict
@@ -28,14 +28,25 @@ for r, line in enumerate(test_input.splitlines()):
 
 t = 0
 antinodes = set()
+antinodes_p2 = set()
 for an in g.keys():
 
     p = permutations(g[an], 2)
     for (x1, y1), (x2, y2) in p:
+        antinodes_p2.add((x1, y1))
+        antinodes_p2.add((x2, y2))
         dx, dy = x1 - x2, y1 - y2
         node = (x1 + dx, y1 + dy)
         if node[0] < 0 or node[0] > bc or node[1] < 0 or node[1] > br:
             continue
+        antinodes_p2.add(node)
+        while True:
+            node = (node[0] + dx, node[1] + dy)
+            if node[0] < 0 or node[0] > bc or node[1] < 0 or node[1] > br:
+                break
+            antinodes_p2.add(node)
+
         antinodes.add(node)
 
 print(len(antinodes))
+print(len(antinodes_p2))
