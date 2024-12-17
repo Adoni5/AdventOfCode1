ae2f8e8ea48f1c7888ec32d6d1ce9ade7123c683
Adoni5 <roryjmunro1@gmail.com>
2024-12-17 14:12:07 +0000

    Day 16 part 2 paths are hard

--- 2024/day16/day16.py
+++ 2024/day16/day16.py
@@ -14,79 +14,62 @@ test_input = """###############
 #S..#.....#...#
 ###############"""
 
-
-from collections import deque
+import heapq
+from collections import defaultdict, deque
 from pprint import pprint
 
-# with open("input.txt") as fh:
-#     test_input = fh.read()
+with open("input.txt") as fh:
+    test_input = fh.read()
 g = {}
-q = deque([])
+q = []
+end = None
 for r, line in enumerate(test_input.splitlines()):
     for c, x in enumerate(line.strip()):
         if x == "S":
-            q.append((c, r, 0, set(), (-1, 0), 0, 0, []))
+            heapq.heappush(q, (0, c, r, (-1, 0), 0, 0))
             x = "."
+        if x == "E":
+            end = (c, r)
         g[(c, r)] = x
 
+lscore = 20000000
 _rot_clock = {(-1, 0): (0, -1), (0, -1): (1, 0), (1, 0): (0, 1), (0, 1): (-1, 0)}
 _rot_anti = {(-1, 0): (0, 1), (0, 1): (1, 0), (1, 0): (0, -1), (0, -1): (-1, 0)}
 fin = []
 visited = set()
+paths = defaultdict(list)
 while q:
-    # print([(c, r, s, d) for c, r, s, _, d in q])
-    c, r, score, v, d, st, rot, path = q.popleft()
-    path = list(path)
+    score, c, r, d, st, ro = heapq.heappop(q)
+    if score > lscore:
+        continue
+    if (c, r, d) in visited:
+        continue
+    visited.add((c, r, d))
+    if g.get((c, r)) == "E":
+        print(st, ro, score)
+        lscore = min(lscore, score)
+        continue
+    dc, dr = d
+    nc, nr = c + dc, r + dr
+    nn = g.get((nc, nr))
 
-    # rotate
-    _d_cl = _rot_clock[d]
-    _d_an = _rot_anti[d]
-    for _d, ro in ((_d_cl, 1), (_d_an, 1), (d, 0)):
-        dc, dr = _d
-        nc, nr = c + dc, r + dr
-        nn = g.get((nc, nr))
-        nrot = rot
-        rscore = score
-        if (nc, nr) in v:
-            # print(f"visited {nc},{nr},{d} from {c},{r}")
-            continue
-        v.add((nc, nr))
-        if ro:
-            nrot = rot + 1
-            rscore = score + 1000
-        if nn == ".":
-            st += 1
-            rscore += 1
-            path.append((nc, nr, _d, st, nrot))
-            if not ro:
-                q.appendleft((nc, nr, rscore, v, _d, st, nrot, path))
-            else:
-                q.append((nc, nr, rscore, v, _d, st, nrot, path))
+    for rot in (_rot_clock[d], _rot_anti[d]):
+        rs = score + 1000
+        paths[(c, r, rs, rot)].append((c, r, score, d))
+        heapq.heappush(q, (rs, c, r, rot, st, ro + 1))
+    if nn != "#":
+        heapq.heappush(q, (score + 1, nc, nr, d, st + 1, ro))
+        paths[(nc, nr, score + 1, d)].append((c, r, score, d))
 
-        elif nn == "E":
-            fin.append((rscore + 1, st, rot, path))
-
-
-def draw(g: dict, path: tuple[int, int]):
-    x = {(0, -1): "^", (0, 1): "v", (1, 0): ">", (-1, 0): "<"}
-    lines = []
-    _r = 0
-    line = []
-    paths = {(c, r): d for c, r, d, *_ in path}
-
-    for (c, r), v in g.items():
-        if r != _r:
-            lines.append("".join(line))
-            line = []
-            _r = r
-        if _x := paths.get((c, r), False):
-            v = x[_x]
-        line.append(v)
-    lines.append("".join(line))
-    print("\n".join(lines))
-    print("\n\n")
-
-
-print(min(fin, key=lambda x: x[0])[0:3])
-print(len(fin))
-draw(g, min(fin, key=lambda x: x[0])[-1])
+print(lscore)
+path = set()
+test = [
+    (*end, lscore, (1, 0)),
+    (*end, lscore, (0, -1)),
+]
+while test:
+    c, r, s, d = test.pop()
+    path.add((c, r))
+    for state in paths[(c, r, s, d)]:
+        test.append(state)
+print(len(path))
