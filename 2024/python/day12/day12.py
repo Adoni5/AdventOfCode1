784294b718629e4722f43cbee145c5fb77dfcee3
Rory Munro <rory.munro@nanoporetech.com>
2024-12-20 13:36:19 +0000

    starting day12 p2

--- 2024/python/day12/day12.py
+++ 2024/python/day12/day12.py
@@ -3,10 +3,10 @@ BBCD
 BBCC
 EEEC"""
 
-with open("input.txt") as fh:
-    test_input = fh.read()
+# with open("input.txt") as fh:
+#     test_input = fh.read()
 g = {}
-from collections import deque
+from collections import defaultdict, deque
 from pprint import pprint
 
 for r, l in enumerate(test_input.splitlines()):
@@ -16,6 +16,7 @@ for r, l in enumerate(test_input.splitlines()):
 s = 0
 d = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 ans = {}
+ans_p2 = 0
 visited_ = set()
 for (c, r), content in g.items():
     visited = set()
@@ -24,6 +25,8 @@ for (c, r), content in g.items():
     perimeter = 0
     region = 1
     q = deque([(c, r)])
+    plot = {(c, r): content}
+
     while q:
         # print(q)
         c, r = q.popleft()
@@ -34,15 +37,10 @@ for (c, r), content in g.items():
             if (nc, nr) in visited:
                 continue
             if g.get((nc, nr), "*") != content:
-                # print(content, c, r)
-                # print(g.get((nc, nr), "*"))
-                # print(f"{content}, perim + 1 for {c}, {r}")
                 perimeter += 1
             else:
                 term = False
-                # print(
-                #     f"incrementing region for {content}, ({c, r}) by visiting {nc}, {nr}"
-                # )
+                plot[(nc, nr)] = content
                 region += 1
                 visited.add((nc, nr))
                 visited_.add((nc, nr))
@@ -50,8 +48,22 @@ for (c, r), content in g.items():
         if term:
             visited.add((c, r))
             # visited_.add((nc, nr))
+    # part 2
+
+    print(plot)
+    v = set()
+    p2_q = deque([plot])
+    while plot:
+        c, r = plot.popleft()
+        pu = pd = pl = pr = 0
+        for dc, dr in d:
+            _c, _r = c, r
+            while True:
+                nc, nr = _c + dc, _r + dr
+                nn = plot.get((nc, nr))
+                if nn 
+
     # print(content, region, perimeter, c, r)
     s += region * perimeter
     ans[content] = (region, perimeter)
 print(s)
-pprint(ans)
