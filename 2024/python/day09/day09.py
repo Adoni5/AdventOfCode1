82379e4c92a6b6e493b51f1bd5a02343c8b6dd06
Rory Munro <rory.munro@nanoporetech.com>
2024-12-20 11:53:18 +0000

    Redopart 9 but smart

--- 2024/python/day09/day09.py
+++ 2024/python/day09/day09.py
@@ -1,7 +1,9 @@
-test_input = "2333133121414131402"
-
-from collections import deque
+from collections import defaultdict, deque
 from itertools import cycle, islice
+from operator import mul
+from tqdm import tqdm
+
+test_input = "2333133121414131402"
 
 
 def roundrobin(*iterables):
@@ -14,19 +16,47 @@ def roundrobin(*iterables):
         yield from map(next, iterators)
 
 
-d = list(zip(enumerate(map(int, test_input))))
+with open("input.txt") as fh:
+    test_input = fh.read()
 
-files = d[::2][::-1]
-print(d)
-id = len(files) - 1
-new_order = []
-b = {}
-for f in files:
-    for i, block in enumerate(d):
-        if i % 2:
-            # f_blokc
-            if block >= f:
-                d[i] -= f
+d = list(map(int, test_input))
+# We need indices for the files
 
-    id -= 1
-print(d)
+# Id, length
+files = list(tuple(enumerate(d[::2])))
+free_blocks = d[1::2]
+ordered = {}
+ptr = -1
+free_block_ptrs = deque([])
+free_block_spans = defaultdict(deque)
+for x in roundrobin(files, free_blocks):
+    if isinstance(x, int):
+        for _ in range(x):
+            free_block_spans[ptr].append(x - _)
+            ptr += 1
+            ordered[ptr] = "."
+            free_block_ptrs.append(ptr)
+
+    elif isinstance(x, tuple):
+        id, fl = x
+        for _ in range(fl):
+            ptr += 1
+            ordered[ptr] = id
+# print(free_block_ptrs)
+# print(ordered)
+
+for k, id in tqdm(ordered.items().__reversed__(), total=len(ordered)):
+    # for k, id in ordered.items().__reversed__():
+    if id == 0 or id == ".":
+        continue
+    # print(k, id)
+    if free_block_ptrs:
+        free_block = free_block_ptrs.popleft()
+        if k > free_block:
+            # print(f"moving {id} from {k} to {free_block}")
+            ordered[k] = "."
+            ordered[free_block] = id
+    else:
+        break
+# print(ordered)
+print(sum(mul(i, id) for (i, id) in enumerate(ordered.values()) if isinstance(id, int)))
