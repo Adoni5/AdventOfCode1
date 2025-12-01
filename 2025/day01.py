0964a8559abb62c2375a05ab1adb2dda1927926d
Rory Munro <rory.munro@nanoporetech.com>
2025-12-01 18:15:39 +0000

    remove prints

--- 2025/day01.py
+++ 2025/day01.py
@@ -7,7 +7,6 @@ ops = {"L": sub, "R": add}
 p1 = 0
 p2 = 0
 for line in sys.stdin.read().splitlines():
-    print(line)
     direct = line[0]
     distance = int(line[1:])
     pass_zero = distance // 100
@@ -23,9 +22,7 @@ for line in sys.stdin.read().splitlines():
         p1 += 1
 
     print(f"The dial is rotated {line} to point at {dial}.")
-    print(f"new passed 0: {pass_zero}")
     p2 += pass_zero
-    print()
 
 print(f"P1: {p1}")
 print(f"P2: {p2}")
