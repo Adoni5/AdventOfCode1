5c6b4a33954fb607a734941b62a0a0088bd25c20
Rory Munro <rory.munro@nanoporetech.com>
2025-12-01 18:15:14 +0000

    day-1 2025

---
+++ 2025/day01.py
@@ -0,0 +1,32 @@
+from operator import sub, add
+import sys
+
+dial = 50
+maxi = 100
+ops = {"L": sub, "R": add}
+p1 = 0
+p2 = 0
+for line in sys.stdin.read().splitlines():
+    print(line)
+    direct = line[0]
+    distance = int(line[1:])
+    pass_zero = distance // 100
+    distance = distance % 100
+
+    if dial == 0 and direct == "L":
+        pass_zero -= 1
+    dial = ops[direct](dial, distance)
+    if dial <= 0 or dial > 99:
+        pass_zero += 1
+    dial = dial % 100
+    if dial == 0:
+        p1 += 1
+
+    print(f"The dial is rotated {line} to point at {dial}.")
+    print(f"new passed 0: {pass_zero}")
+    p2 += pass_zero
+    print()
+
+print(f"P1: {p1}")
+print(f"P2: {p2}")
+
