a7aa3787db298e93208af17a27ee8114e0215b53
Rory Munro <rory.munro@nanoporetech.com>
2025-12-02 08:15:53 +0000

    Day02 p2

--- 2025/day02/day02.py
+++ 2025/day02/day02.py
@@ -2,15 +2,26 @@ import sys
 from itertools import batched
 
 invalids = []
+p2_invalids = []
 ids = sys.stdin.read().split(",")
 for id in ids:
-    start, end = tuple(map(int, id.split("-")))
+    start, end = tuple(map(int, id.split("-") ))
     ranges = tuple(map(str, range(start, end + 1)))
     for r in ranges:
         if not len(r) % 2:
-            h1, h2 = batched(r, len(r)//2)
-            if h1 == h2:
-                print(f"Invalid {r}")
+            # part 1
+            b = set(batched(r, len(r)//2))
+            if len(b) == 1:
                 invalids.append(int(r))
+        # part 2
+        for _ in range(1, (len(r)//2) + 1):
+            b = set(batched(r, _))
+            if len(b) == 1:
+                p2_invalids.append(int(r))
+                break
+
+
+            
 print(sum(invalids))
+print(sum(p2_invalids))
 
